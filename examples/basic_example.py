0b7dee8d6e58c551a3a5ff1f951cd864815a94c5
Jan von Rickenbach <jan.vonrickenbach@ansys.com>
2022-08-09 17:23:38 +0200

    Test start of dpf server

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -10,4 +10,8 @@ Very basic!
 out = "Hello World"
 
 #####################
-out
+import ansys.dpf.core as dpf
+
+# depending on your scenario you might also need to start the dpf server
+# for instance server = dpf.start_local_server()
+server = dpf.server.connect_to_server("127.0.0.1", port=21002)
