d07e29f982fee68aacb193198b2db1a2fa11030c
Rene Roos <rene.roos@ansys.com>
2022-08-16 10:00:42 +0200

    add Core Shear failure criterion

---
+++ src/ansys/dpf/composites/failure_criteria/core_failure.py
@@ -0,0 +1,35 @@
+
+from .failure_criteion_base import FailureCriterionBase
+
+ATTRS_CORE_FAILURE = ["include_ins", "wf"]
+
+class CoreFailureCriterion(FailureCriterionBase):
+    """
+    Defines the core shear failure criterion for core materials like foam and honeycomb.
+    """
+
+    def __init__(self,
+                 include_ins: bool = False,
+                 wf: float = 1.
+                 ):
+
+        super().__init__(name="Core Failure", active=True)
+
+        for attr in ATTRS_CORE_FAILURE:
+            setattr(self, attr, eval(attr))
+
+    def _get_include_ins(self) -> bool:
+        return self._include_ins
+    def _set_include_ins(self, value: bool):
+        self._include_ins = value
+
+    def _get_wf(self) -> float:
+        return self._wf
+    def _set_wf(self, value: float):
+        self._wf = value
+
+    include_ins = property(_get_include_ins, _set_include_ins,
+                         doc="Activate this option to enable the formulation which "
+                             "considers interlaminar normal stresses.")
+    wf = property(_get_wf, _set_wf,
+                         doc="Weighting factor of the failure mode (cs).")
