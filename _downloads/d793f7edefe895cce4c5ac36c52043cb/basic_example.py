127efe59baba4cc8e76a9434b0bd420eab37432f
janvonrickenbach <janvonrickenbach@users.noreply.github.com>
2022-08-16 07:32:11 +0000

    Deploying to gh-pages from @ pyansys/pydpf-composites@de5319c043d57058dd16af86210ea362949cfc6a 🚀

--- _downloads/d793f7edefe895cce4c5ac36c52043cb/basic_example.py
+++ _downloads/d793f7edefe895cce4c5ac36c52043cb/basic_example.py
@@ -10,125 +10,13 @@ Very basic!
 
 import json
 import os
+import pathlib
 
 #####################
 import ansys.dpf.core as dpf
 
-
-def get_failure_criteria_definition():
-    max_strain_json_configuration = {
-        "active": True,
-        "e1": True,
-        "e12": True,
-        "e13": False,
-        "e2": True,
-        "e23": False,
-        "e3": False,
-        "eSxy": 0.015,
-        "eSxz": 0.015,
-        "eSyz": 0.015,
-        "eXc": -0.008,
-        "eXt": 0.01,
-        "eYc": -0.008,
-        "eYt": 0.01,
-        "eZc": -0.008,
-        "eZt": 0.01,
-        "force_global_strain_limits": False,
-        "wf_e1": 1,
-        "wf_e12": 1,
-        "wf_e13": 1,
-        "wf_e2": 1,
-        "wf_e23": 1,
-        "wf_e3": 1,
-    }
-    max_stress_json_configuration_2d = {
-        "active": True,
-        "s1": True,
-        "s12": True,
-        "s13": False,
-        "s2": True,
-        "s23": False,
-        "s3": False,
-        "wf_s1": 1,
-        "wf_s12": 1,
-        "wf_s13": 1,
-        "wf_s2": 1,
-        "wf_s23": 1,
-        "wf_s3": 1,
-    }
-    quadratic_failure_json_configuration_2d = {"active": True, "dim": 2, "wf": 1}
-    hashin_json_configuration = {
-        "active": True,
-        "dim": 2,
-        "hd": True,
-        "hf": True,
-        "hm": True,
-        "wf_hd": 1.0,
-        "wf_hf": 1.0,
-        "wf_hm": 1.0,
-    }
-    cuntze_failure_json_configuration = {
-        "active": True,
-        "b21": 0.2,
-        "b32": 1.38,
-        "cfc": True,
-        "cft": True,
-        "cma": True,
-        "cmb": True,
-        "cmc": True,
-        "dim": 2,
-        "fracture_plane_angle": 53,
-        "mode_interaction_coeff": 2.6,
-        "wf_cfc": 1,
-        "wf_cft": 1,
-        "wf_cma": 1,
-        "wf_cmb": 1,
-        "wf_cmc": 1,
-    }
-
-    core_failure_json_configuration = {"active": True, "include_ins": False, "wf": 1}
-
-    von_mises_only_strain_json_configuration = {
-        "active": True,
-        "vme": True,
-        "vms": False,
-        "wf_vme": 1.0,
-        "wf_vms": 1.0,
-    }
-
-    return {
-        "max_strain": max_strain_json_configuration,
-        "max_stress": max_stress_json_configuration_2d,
-        "hashin": hashin_json_configuration,
-        "tsai_hill": quadratic_failure_json_configuration_2d,
-        "tsai_wu": quadratic_failure_json_configuration_2d,
-        "hoffman": quadratic_failure_json_configuration_2d,
-        "core_failure": core_failure_json_configuration,
-        "von_mises": von_mises_only_strain_json_configuration,
-        "cuntze": cuntze_failure_json_configuration,
-    }
-
-
-def get_pin_config():
-    return {
-        "input": {
-            "data_source": 4,
-            "property": 13,
-            "mesh_region": 7,
-            "configuration": 100,
-            "strains": 0,
-            "stresses": 1,
-            "materials_container": 23,
-            "material_support": 21,
-        },
-        "output": {
-            "min": 0,
-            "max": 1,
-            "mode_index": 0,
-            "value_index": 1,
-        },
-    }
-
+from ansys.dpf.composites.failure_config import get_failure_criteria_definition
+from ansys.dpf.composites.pin_config import get_pin_config
 
 # depending on your scenario you might also need to start the dpf server
 # for instance server = dpf.start_local_server()
@@ -136,9 +24,12 @@ server = dpf.server.connect_to_server("127.0.0.1", port=21002)
 dpf.load_library("libcomposite_operators.so", "composites")
 dpf.load_library("libAns.Dpf.EngineeringData.so", "engineeringdata")
 
-rst_path = os.path.join(r"shell.rst")
-h5_path = os.path.join(r"ACPCompositeDefinitions.h5")
-material_path = os.path.join(r"material.engd")
+TEST_DATA_ROOT_DIR = pathlib.Path(os.environ["REPO_ROOT"]) / "tests" / "data"
+
+# Todo make files available for users
+rst_path = os.path.join(TEST_DATA_ROOT_DIR, "shell.rst")
+h5_path = os.path.join(TEST_DATA_ROOT_DIR, "ACPCompositeDefinitions.h5")
+material_path = os.path.join(TEST_DATA_ROOT_DIR, "material.engd")
 
 rst_server_path = dpf.upload_file_in_tmp_folder(rst_path, server=server)
 h5_server_path = dpf.upload_file_in_tmp_folder(h5_path, server=server)
