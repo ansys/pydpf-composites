a83c552196bbb39814e2bebbfca920755c48bd72
Jan von Rickenbach <jan.vonrickenbach@ansys.com>
2022-08-22 13:33:18 +0200

    Add dev environment

--- src/ansys/dpf/composites/__init__.py
+++ src/ansys/dpf/composites/__init__.py
@@ -7,6 +7,6 @@ composites
 try:
     import importlib.metadata as importlib_metadata
 except ModuleNotFoundError:
-    import importlib_metadata
+    import importlib_metadata  # type: ignore
 
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
