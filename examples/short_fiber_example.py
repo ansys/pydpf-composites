510e2a70460dbba846066e7271e28b9f931ba3d9
jvonrick <jan.vonrickenbach@ansys.com>
2022-10-11 09:31:04 +0200

    Use inputs instead of pins

--- examples/short_fiber_example.py
+++ examples/short_fiber_example.py
@@ -43,22 +43,21 @@ data_sources.set_result_file_path(rst_server_path)
 # %%
 # Create dpf model and mesh provider
 model = dpf.Model(rst_server_path)
-metadata = model.metadata
 
 # %%
 # Short Fiber Failure Criterion Evaluator
 sf_op = dpf.Operator("composite::short_fiber_failure_criterion_evaluator")
 sf_op.inputs.data_sources(data_sources)
-sf_op.connect(2, "ultimate")  # "yield" or "ultimate" (default)
+sf_op.inputs.stress_limit_type("ultimate")  # "yield" or "ultimate" (default)
 sf_op.run()
 
 mat_support_operator = dpf.Operator("mat_support_provider")
 mat_support_operator.connect(4, data_sources)
 
 minmax_per_element = dpf.Operator("composite::minmax_per_element_operator")
-minmax_per_element.connect(0, sf_op, 0)
+minmax_per_element.inputs.fields_container(sf_op)
 minmax_per_element.inputs.mesh(model.metadata.meshed_region)
-minmax_per_element.connect(21, mat_support_operator, 0)
+minmax_per_element.inputs.abstract_field_support(mat_support_operator)
 max_element_stress_cont = minmax_per_element.get_output(1, dpf.types.fields_container)
 fc_mode = max_element_stress_cont[0]
 fc_value = max_element_stress_cont[1]
