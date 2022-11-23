05c2a86b44f340a714d5205eadca784aca5a6a13
jvonrick <jan.vonrickenbach@ansys.com>
2022-11-23 11:44:08 +0100

    increase timeout some more

--- tests/conftest.py
+++ tests/conftest.py
@@ -282,7 +282,7 @@ def dpf_server(request: pytest.FixtureRequest):
             )
 
     with start_server_process() as server_process:
-        server = dpf.server.connect_to_server(port=server_process.port, timeout=10)
+        server = dpf.server.connect_to_server(port=server_process.port, timeout=40)
 
         wait_until_server_is_up(server)
         load_composites_plugin(platform=server_process.platform, server=server)
