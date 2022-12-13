0562f94ff895b6df7c6d48cf0c3dd8e01589da68
jvonrick <jan.vonrickenbach@ansys.com>
2022-12-13 15:52:43 +0100

    Use alias instead of NewType so the documentation looks ok.

--- src/ansys/dpf/composites/material.py
+++ src/ansys/dpf/composites/material.py
@@ -1,6 +1,9 @@
 """Material types."""
-from typing import NewType
 
-import numpy as np
+from numpy import int64
 
-MaterialId = NewType("MaterialId", np.int64)
+# Note: Ideally this would be NewType
+# but it looks like the support in sphinx for that is not good.
+"""Alias for material id."""
+MaterialId = int64
+"""Alias for material id."""
