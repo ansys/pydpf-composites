0c74948bb41796fc071b59396e43d1cb1b20a432
Rene Roos <rene.roos@ansys.com>
2023-01-13 15:56:07 +0100

    update doc string

--- src/ansys/dpf/composites/failure_criteria/von_mises.py
+++ src/ansys/dpf/composites/failure_criteria/von_mises.py
@@ -62,7 +62,7 @@ class VonMisesCriterion(FailureCriterionBase):
     eval_ins = property(
         _get_eval_ins,
         _set_eval_ins,
-        doc="Whether to consider s3 (compute s3 for layered shells).",
+        doc="Whether to consider interlaminar normal stresses (s3) and compute them for layered shells.",
     )
 
 
