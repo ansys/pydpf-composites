bba98cef1b0a96684e6658fc7396c879e77a4d6b
Dominik Gresch <greschd@users.noreply.github.com>
2023-01-25 12:05:06 +0100

    Use 'PyDPF Composites' as HTML title (#133)
    Use the capitalized 'PyDPF Composites' instead of 'pydpf-composites'
    for the 'html_title' and 'html_short_title' in the documentation.
    
    This is shown in the tab name, and the breadcrumbs.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ release = version = __version__
 html_logo = pyansys_logo_black
 html_favicon = ansys_favicon
 html_theme = "ansys_sphinx_theme"
-html_short_title = html_title = "pydpf-composites"
+html_short_title = html_title = "PyDPF Composites"
 
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
