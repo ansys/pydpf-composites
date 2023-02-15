131fb88e2521c14ce335ee743d6794a63328f25d
jvonrick <jan.vonrickenbach@ansys.com>
2023-02-15 17:24:44 +0100

    Print path with prefix

--- tests/basic_workflow_test.py
+++ tests/basic_workflow_test.py
@@ -15,6 +15,7 @@ def test_single_operator(dpf_server):
     if not dpf_server.local_server:
         rst_path = dpf.upload_file_in_tmp_folder(rst_path, server=dpf_server)
     rst_data_source = dpf.DataSources()
+    print(f"rst path: {rst_path}")
     rst_data_source.set_result_file_path(rst_path, "rst")
     mesh_provider.inputs.data_sources(rst_data_source)
 
