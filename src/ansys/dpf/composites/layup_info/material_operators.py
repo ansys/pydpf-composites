efb650079ab34f6df924bf05cd34e05f684c4914
jvonrick <jan.vonrickenbach@ansys.com>
2023-02-28 13:48:38 +0100

    Add deprecations warnings

--- src/ansys/dpf/composites/layup_info/material_operators.py
+++ src/ansys/dpf/composites/layup_info/material_operators.py
@@ -1,5 +1,6 @@
 """Helper function to get material-related operators."""
-from dataclasses import dataclass
+from typing import Optional
+from warnings import warn
 
 from ansys.dpf.core import DataSources, Operator
 
@@ -8,7 +9,6 @@ __all__ = ("MaterialOperators", "get_material_operators")
 from ansys.dpf.composites.unit_system import UnitSystemProvider
 
 
-@dataclass(frozen=True)
 class MaterialOperators:
     """Provides the container for material-related operators.
 
@@ -24,16 +24,48 @@ class MaterialOperators:
         evaluate material properties. This container cannot be
         queried in Python, but it can be passed to other DPF operators
         that evaluate the properties.
+    result_info_provider:
+        Provides the ``ResultInfo`` object.
     """
 
-    material_provider: Operator
-    material_support_provider: Operator
+    def __init__(
+        self,
+        material_provider: Operator,
+        material_support_provider: Operator,
+        result_info_provider: Operator,
+    ):
+        """Create a MaterialOperators object."""
+        self._material_provider = material_provider
+        self._material_support_provider = material_support_provider
+        self._result_info_provider = result_info_provider
+
+    @property
+    def result_info_provider(self) -> Operator:
+        """Get result_info_provider."""
+        warn(
+            "Getting the result_info_provider from a "
+            "MaterialsOperators object is deprecated. "
+            "Use get_unit_system to obtain the unit system.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return self._result_info_provider
+
+    @property
+    def material_support_provider(self) -> Operator:
+        """Get material_support_provider."""
+        return self._material_support_provider
+
+    @property
+    def material_provider(self) -> Operator:
+        """Get material_provider."""
+        return self._material_provider
 
 
 def get_material_operators(
     rst_data_source: DataSources,
     engineering_data_source: DataSources,
-    unit_system: UnitSystemProvider,
+    unit_system: Optional[UnitSystemProvider] = None,
 ) -> MaterialOperators:
     """Get material properties related to operators.
 
@@ -52,6 +84,19 @@ def get_material_operators(
     material_support_provider.inputs.property("mat")
     material_support_provider.inputs.data_sources(rst_data_source)
 
+    result_info_provider = Operator("ResultInfoProvider")
+    result_info_provider.inputs.data_sources(rst_data_source)
+
+    if unit_system is None:
+        warn(
+            "Calling get_material_operators without "
+            "a unit_system argument is deprecated. Use get_unit_system "
+            "to obtain the unit_system",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        unit_system = result_info_provider.outputs.result_info
+
     # Set up material provider
     # Combines the material support in the engineering data XML file and the unit system.
     # Its output can be used to evaluate material properties.
@@ -66,4 +111,5 @@ def get_material_operators(
     return MaterialOperators(
         material_provider=material_provider,
         material_support_provider=material_support_provider,
+        result_info_provider=result_info_provider,
     )
