526e4c621053dc77c5a9cf1d62f919409815257a
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-03-21 20:03:00 +0100

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -46,7 +46,7 @@ html_theme_options = {
     "switcher": {
         "json_url": f"https://{cname}/versions.json",
         "version_match": get_version_match(__version__),
-    }
+    },
 }
 
 # Sphinx extensions
