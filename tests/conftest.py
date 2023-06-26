0003774ce09906d5a5d1238de27261aff18205d3
Rene Roos <rene.roos@ansys.com>
2023-06-26 12:02:57 +0200

    Use latest container again.

--- tests/conftest.py
+++ tests/conftest.py
@@ -101,7 +101,7 @@ class DockerProcess:
         process_out_file: pathlib.Path,
         process_err_file: pathlib.Path,
         license_server: str = "",
-        image_name: str = "ghcr.io/ansys/pydpf-composites:assemblies",  # revert before merge
+        image_name: str = "ghcr.io/ansys/pydpf-composites:latest",
         mount_directories: Mapping[str, str] = MappingProxyType({}),
     ):
         """Initialize the wrapper
