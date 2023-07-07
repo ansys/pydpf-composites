eb15fa76706254956dedf463e9c7df9f35a13562
janvonrickenbach <jan.vonrickenbach@ansys.com>
2023-07-07 09:22:14 +0200

    Update examples/008_assembly_example.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- examples/008_assembly_example.py
+++ examples/008_assembly_example.py
@@ -59,7 +59,7 @@ irf_field.plot()
 # and one with a "solid" label. For dpf versions older than version 7.0,
 # the layup properties must be queried with the correct composite definition label. The code
 # following gets element information for all layered elements.
-# For newer server versions, the element_info can be retrieved directly.
+# For DPF Server versions 7.0 and later, element information can be retrieved directly.
 
 if version_older_than(server, "7.0"):
     element_infos = []
