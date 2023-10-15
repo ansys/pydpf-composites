98251bd546d845ea21661346f5c8716d21bd1f2b
René Roos <rene.roos@ansys.com>
2023-10-15 22:38:11 +0200

    implement remark from the code review.

--- src/ansys/dpf/composites/select_indices.py
+++ src/ansys/dpf/composites/select_indices.py
@@ -57,7 +57,7 @@ def get_selected_indices(
     -----
     Returns an empty selection if any of the collections is empty.
 
-    The indices (nodes, layers and spots) are 0-based. Pay attention to this
+    The indices (nodes, layers, and spots) are 0-based. Pay attention to this
     when using the "composite::minmax_per_element_operator" where the
     min/max layer indices are 1-based starting with Workbench 2024 R1
     (DPF server 7.1).
