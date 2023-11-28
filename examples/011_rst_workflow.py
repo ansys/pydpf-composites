cc41780d28a67291a6858fe3067ece4483a921b4
René Roos <105842014+roosre@users.noreply.github.com>
2023-11-28 14:41:16 +0100

    Update examples/011_rst_workflow.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- examples/011_rst_workflow.py
+++ examples/011_rst_workflow.py
@@ -20,8 +20,8 @@ or ACP (Ansys Composite PrePost) standalone.
 
 You can set the material UUID in Mechanical APDL with the ``MP,UVID,<material index>,<value>`` command.
 
-This workflow is supported since version 2024 R2 (DPF Server version 8.0). A few advanced features are
-not supported with the RST onl workflow. Refer to Section :ref:`limitations` for details.
+This workflow is supported in 2024 R2 (DPF Server version 8.0) and later. A few advanced features are
+not supported with the RST onl workflow. For more information, see :ref:`limitations`.
 """
 # %%
 # Set up analysis
