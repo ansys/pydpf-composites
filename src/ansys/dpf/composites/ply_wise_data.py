dfa7f6afa01b33e903b1353fc98854d51d229b39
jvonrick <jan.vonrickenbach@ansys.com>
2024-01-03 16:55:26 +0100

    Assign correct location

--- src/ansys/dpf/composites/ply_wise_data.py
+++ src/ansys/dpf/composites/ply_wise_data.py
@@ -63,6 +63,7 @@ def get_ply_wise_data(
     elemental_nodal_data = filter_ply_data_op.outputs.field()
 
     if requested_location == locations.elemental_nodal:
+        elemental_nodal_data.location = locations.elemental_nodal
         return elemental_nodal_data
 
     if requested_location == locations.elemental:
