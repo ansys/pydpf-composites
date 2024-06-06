2ca83ccfae01ff8ecf911efee3c8dd8eff334dc5
René Roos <rene.roos@ansys.com>
2024-06-06 11:45:24 +0200

    cosmetic change

--- src/ansys/dpf/composites/layup_info/_layup_info.py
+++ src/ansys/dpf/composites/layup_info/_layup_info.py
@@ -326,7 +326,7 @@ def get_analysis_ply_index_to_name_map(
         DPF Meshed region enriched with lay-up information
 
     Note: Analysis plies of ACP's imported solid model which are linked
-    to homogeneous elements only are ignored currently.
+    to homogeneous elements only are currently skipped.
     """
     analysis_ply_name_to_index_map = {}
     with mesh.property_field("layer_to_analysis_ply").as_local_field() as local_field:
