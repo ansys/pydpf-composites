f0018a9661c3f4dd5f01330d963e879e2ce06311
René Roos <rene.roos@ansys.com>
2025-03-03 09:06:22 +0100

    extend the rst workflow example: show how to compute and access layer-wise results of a failure evaluation.

--- examples/011_rst_workflow.py
+++ examples/011_rst_workflow.py
@@ -40,7 +40,8 @@ material UUIDs in MAPDL.
 
 .. important::
    The material UUIDs in the engineering data file must be identical
-   to the UUIDs in Mechanical APDL (RST file).
+   to the UUIDs in Mechanical APDL (RST file). A detailed explanation
+   can be found at the end of this example.
 
 The postprocessing of MAPDL models is supported in 2024 R2 (DPF Server version 8.0)
 and later. A few advanced features are not supported with the RST only workflow.
@@ -72,7 +73,9 @@ from ansys.dpf.composites.failure_criteria import (
     MaxStressCriterion,
     VonMisesCriterion,
 )
+from ansys.dpf.composites.select_indices import get_selected_indices
 from ansys.dpf.composites.server_helpers import connect_to_or_start_server
+import ansys.dpf.core as dpf
 
 # %%
 # Start a DPF server and copy the example files into the current working directory.
@@ -121,6 +124,56 @@ sampling_point = composite_model.get_sampling_point(combined_criterion=combined_
 sampling_plot = sampling_point.get_result_plots(core_scale_factor=0.1)
 sampling_plot.figure.show()
 
+# %% Layer-wise failure criteria
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# The next lines show how to compute layer-wise failure criteria
+# and how to access the data of each layer.
+
+# %%
+# Create a combined failure criterion
+combined_fc = CombinedFailureCriterion(
+    name="My Failure Criteria",
+    failure_criteria=[
+        MaxStrainCriterion(),
+        MaxStressCriterion(),
+    ],
+)
+
+# %%
+# Prepare and configure inputs for the multiple failure criteria operator
+# Note: the output has the same format as the input
+# (strain and stress fields). So there is one failure value and mode for each
+# integration point, layer and element, for all time steps.
+strain_operator = composite_model.core_model.results.elastic_strain()
+strain_operator.inputs.bool_rotate_to_global(False)
+
+stress_operator = composite_model.core_model.results.stress()
+stress_operator.inputs.bool_rotate_to_global(False)
+
+failure_evaluator = dpf.Operator("composite::multiple_failure_criteria_operator")
+failure_evaluator.inputs.configuration(combined_fc.to_json())
+failure_evaluator.inputs.materials_container(composite_model.material_operators.material_provider.outputs)
+failure_evaluator.inputs.strains_container(strain_operator.outputs.fields_container)
+failure_evaluator.inputs.stresses_container(stress_operator.outputs.fields_container)
+failure_evaluator.inputs.mesh(composite_model.get_mesh())
+irf_field = failure_evaluator.outputs.fields_container.get_data().get_field({"failure_label": FailureOutput.FAILURE_VALUE, "time": 1})
+failure_mode_field = failure_evaluator.outputs.fields_container.get_data().get_field({"failure_label": FailureOutput.FAILURE_MODE, "time": 1})
+
+# %%
+# Access the data of each layer. Details about how to store custom
+# data in a DPF field is shown at the end of
+# :ref:`Material properties and custom failure criterion <sphx_glr_examples_gallery_examples_004_get_material_properties_example.py>`.
+
+for element_id in irf_field.scoping.ids:
+    irf_data = irf_field.get_entity_data_by_id(element_id)
+    model_data = failure_mode_field.get_entity_data_by_id(element_id)
+    element_info = composite_model.get_element_info(element_id)
+    element_max = 0
+    for layer_index, dpf_material_id in enumerate(element_info.dpf_material_ids):
+        selected_indices = get_selected_indices(element_info, layers=[layer_index])
+        layer_irf_values = irf_data[selected_indices]
+        layer_mode_values = model_data[selected_indices]
+        # Do something with the layer data
 
 # %%
 # Create Engineering Data file and set material UUIDs in MAPDL
