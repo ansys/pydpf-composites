38e6b555a43da8a95806076c2e32b1590a5b4487
René Roos <rene.roos@ansys.com>
2025-03-21 14:02:45 +0100

    [WIP] work on examples

--- examples/016_lsdyna_bird_strike.py
+++ examples/016_lsdyna_bird_strike.py
@@ -14,17 +14,17 @@ import json
 
 server = connect_to_or_start_server(ansys_path=r'D:\ANSYSDev\dpf_composites\fake_WB_installation')
 
-solver_dir = r'D:\ANSYSDev\dpf_composites\test_data\lsdyna\shell_model_2'
+solver_dir = r'D:\tmp\SPH_CompositeWing_22R2_files\dp0\SYS-4\MECH'
 
 composite_files = ContinuousFiberCompositesFiles(
     files_are_local=True,
     rst=[os.path.join(solver_dir, 'd3plot')],
     composite={"shell": CompositeDefinitionFiles(
         mapping=None,
-        definition=os.path.join(solver_dir, 'ACPCompositeDefinitions.h5'),
+        definition=os.path.join(solver_dir, 'Setup 3', 'ACPCompositeDefinitions.h5'),
     )
     },
-    engineering_data=os.path.join(solver_dir, 'material.engd'),
+    engineering_data=os.path.join(solver_dir, 'MatML.xml'),
     solver_input_file=os.path.join(solver_dir, 'input.k')
 )
 
@@ -34,10 +34,8 @@ composite_model = CompositeModel(
     default_unit_system=unit_systems.solver_nmm
 )
 
-element_info_1 = composite_model.get_element_info(1)
-element_info_2 = composite_model.get_element_info(2)
-print(element_info_1)
-print(element_info_2)
+element_info_1740 = composite_model.get_element_info(1740)
+print(element_info_1740)
 
 
 stress_operator = composite_model.core_model.results.stress()
@@ -65,7 +63,7 @@ strip_operator.inputs.mesh(composite_model.get_mesh())
 
 stripped_stress_field = strip_operator.get_output(
     pin=0, output_type=dpf.types.fields_container
-)[0]
+)[-1]
 
 # %%
 # Filter data by analysis ply
@@ -87,4 +85,26 @@ for ply_name in all_ply_names:
         requested_location=dpf.locations.elemental,
     )
 
-    composite_model.get_mesh().plot(elemental_values)
+    #composite_model.get_mesh().plot(elemental_values)
+
+
+history_var_op = Operator("lsdyna::d3plot::history_var")
+history_var_op.inputs.data_sources(composite_model.data_sources.rst)
+history_var_op.inputs.time_scoping(stress_container.get_time_scoping())
+history_var = history_var_op.outputs.history_var
+history_var_container = history_var.get_data()
+hv_ids = history_var_container.get_label_scoping("ihv").ids
+print(f"History variables: {hv_ids}")
+time_ids = history_var_container.get_time_scoping().ids
+print(f"Time IDs: {time_ids}")
+for hv_id in hv_ids:
+    label_space = {"ihv":hv_id, "time":time_ids[-1]}
+    field = history_var_container.get_field(label_space)
+
+    composite_model.get_mesh().plot(field)
+
+
+strip_operator.inputs.fields_container(history_var_container)
+prepared_ihv = strip_operator.get_output(
+    pin=0, output_type=dpf.types.fields_container
+)
